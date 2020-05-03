6d5b74d2deedfc149ab4459803fab83f0d53ccc0
MrFlynn <nick@pleatsikas.me>
2020-05-02 21:38:48 -0700

    Worker updates.
    Made a couple of small internal type and visibility switches.

--- src/main/java/snowball/Worker.java
+++ src/main/java/snowball/Worker.java
@@ -5,30 +5,31 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
 import java.io.IOException;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.logging.Logger;
 
 public class Worker implements Runnable {
-    private final LinkedBlockingQueue<String> input;
+    private final LinkedBlockingQueue<URL> input;
     private final LinkedBlockingQueue<String> output;
-    private final String outputDir;
+    private final Path outputDir;
 
     private final static Logger log = Logger.getLogger(Worker.class.getName());
 
-    public Worker(LinkedBlockingQueue<String> input, LinkedBlockingQueue<String> output, String outputDir) {
+    public Worker(LinkedBlockingQueue<URL> input, LinkedBlockingQueue<String> output, Path outputDir) {
         this.input = input;
         this.output = output;
         this.outputDir = outputDir;
     }
 
-    private Optional<Document> getDocument(String url) {
+    private Optional<Document> getDocument(URL url) {
         try {
-            return Optional.ofNullable(Jsoup.connect(url).get());
+            return Optional.ofNullable(Jsoup.connect(url.toString()).get());
         } catch (Exception e) {
             log.warning(String.format("Could not get document %s because %s", url, e));
         }
@@ -36,8 +37,8 @@ public class Worker implements Runnable {
         return Optional.empty();
     }
 
-    private void saveFile(Document doc) {
-        Path finalPath = Paths.get(this.outputDir, doc.location());
+    public void saveFile(Document doc) {
+        Path finalPath = this.outputDir.resolve(doc.location());
         try {
             Files.writeString(finalPath, doc.html());
         } catch (IOException e) {
@@ -45,11 +46,11 @@ public class Worker implements Runnable {
         }
     }
 
-    private ArrayList<String> getLinks(Document doc) {
-        ArrayList<String> urls = new ArrayList<>();
+    public List<String> getLinks(Document doc) {
+        List<String> urls = new ArrayList<>();
 
         for (Element link : doc.select("a[href]")) {
-            urls.add(link.attr("abs:href"));
+            urls.add(link.attr("href"));
         }
 
         return urls;
@@ -60,7 +61,7 @@ public class Worker implements Runnable {
         try {
             //noinspection InfiniteLoopStatement
             while (true) {
-                String url = this.input.take();
+                URL url = this.input.take();
 
                 Optional<Document> doc = this.getDocument(url);
                 if (doc.isPresent()) {
