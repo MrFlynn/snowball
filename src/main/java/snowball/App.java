e76e339f8a11bc3c83606c6cb6f34512dac002a4
MrFlynn <nick@pleatsikas.me>
2020-05-07 19:51:09 -0700

    Marked class variables as private.
    Removed some debugging code.

--- src/main/java/snowball/App.java
+++ src/main/java/snowball/App.java
@@ -1,5 +1,7 @@
 package snowball;
 
+import java.io.File;
+import java.nio.file.Path;
 import java.util.concurrent.Callable;
 
 import picocli.CommandLine;
@@ -15,45 +17,37 @@ class App implements Callable<Integer> {
         names = { "--max-depth", "--hops-away" },
         description = "Maximum crawler depth. Defaults to 6 if not specified."
     )
-    public Integer maxDepth = 6;
+    private Integer maxDepth = 6;
 
     @Option(
         names = { "--max-pages", "--num-pages", "-p" },
         description = "Maximum pages to crawl. Defaults to 10000 if not specified."
     )
-    public Integer maxPages = 10000;
+    private Integer maxPages = 10000;
 
     @Option(
         names = "--output-dir",
         description = "Output directory. Required.",
         required = true
     )
-    public String outputDir;
+    private Path outputDir;
 
     @Option(
         names = "--threads",
         description = "Total number of threads, including the manager thread. Defaults to 4 if not specified."
     )
-    public Integer threads = 4;
+    private Integer threads = 4;
 
     @Option(
         names = { "--seed", "-s", "--seed-file" },
         description = "Path to the seed.txt file. Defaults to seeds.txt if not specified."
     )
-    public String seedFile = "seeds.txt";
+    private File seedFile = new File("./seeds.txt");
 
     @Override
     public Integer call() throws Exception {
-        System.out.printf("%d, %d, %s, %d, %s", maxDepth, maxPages, outputDir, threads, seedFile);
+        // Add manager entrypoint here...
         return 0;
-    }
-
-    public String showArgs() {
-        return "maxDepth:  " + Integer.toString(maxDepth) + "\n" +
-                "maxPages:  " + Integer.toString(maxPages) + "\n" +
-                "outputDir: " + outputDir + "\n" +
-                "threads:   " + Integer.toString(threads) + "\n" +
-                "seedFile:  " + seedFile;
     }
 
     public static void main(String ...args) {
